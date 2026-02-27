85ad081bfee4810bc3ff8ee64be038396fffb66b
Gulfviruz <gulfviruz@gmail.com>
2026-02-27 14:58:10 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const burger = document.getElementById("burger");
-  const navbar = document.getElementById("navbar");
-  const overlay = document.getElementById("overlay");
+const navbar = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
 
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
+burger.addEventListener("click", () => {
+      burger.classList.toggle("active");
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
   });
 
-  overlay.addEventListener("click", () => {
-    burger.classList.remove("active");
-    navbar.classList.remove("active");
-    overlay.classList.remove("active");
+overlay.addEventListener("click", () => {
+      burger.classList.remove("active");
+      navbar.classList.remove("active");
+      overlay.classList.remove("active");
   });
 
-  document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
 
-    form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => {
     
         event.preventDefault();
 
@@ -36,15 +36,14 @@ const burger = document.getElementById("burger");
             }
         });
 
-        const catName = formData.get('cat-name');
-        const email = formData.get('email');
+const catName = formData.get('cat-name');
+const email = formData.get('email');
 
         if (!email) {
             alert('Пожалуйста, укажите E-mail владельца');
             return;
         }
-
-        console.log('Данные котика готовы к отправке:', data);
+console.log('Данные котика готовы к отправке:', data);
         
         alert(`Спасибо! Заявка для кота по имени ${data['cat-name'] || 'Барсик'} отправлена.`);
     });
